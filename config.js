8520cef9f5c43af180d71d5588fe0eb748819dba
Alejandro <acorrea7412@gmail.com>
2023-04-05 22:18:08 -0500

    Add constants for API endpoints

---
+++ config.js
@@ -0,0 +1,5 @@
+export const IMAGE = 'https://image.tmdb.org/t/p/w200'
+export const API_KEY = import.meta.env.VITE_APi_KEY
+export const BASE_URL = 'https://api.themoviedb.org/3/movie/'
+
+export const POPULAR_MOVIES_URL = `${BASE_URL}popular?api_key=${API_KEY}&language=en-US`
