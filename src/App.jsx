d9338ca77dbb64e68be5a846f44da51fb5eb1ada
Alejandro <acorrea7412@gmail.com>
2023-04-10 16:08:03 -0500

    Remove unnecessary  code

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,17 @@ import './App.css'
 import { TopMovies } from './pagination/TopMovies'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { MovieDetails } from './pagination/MovieDetails'
-import { NavBar } from './components/Navbar'
+// import { NavBar } from './components/Navbar'
 
 function App () {
   return (
     <main className='flex flex-col w-full text-font_color p-6'>
-      <NavBar />
+      {/* <NavBar /> */}
 
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<TopMovies />} />
           <Route path='/details/:moviesId' element={<MovieDetails />} />
-          {/* 👇️ only match this when no other routes match */}
           <Route
             path='*'
             element={<TopMovies />}
